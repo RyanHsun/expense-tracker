f9f92e27ac520c59f0288bf698db8f1d00f3ac6e
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-23 11:20:55 +0800

    feat: add mongoose

---
+++ models/record.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    requires: true
+  },
+  category: {
+    type: String,
+    requires: true
+  },
+  date: {
+    type: String,
+    requires: true
+  },
+  amount: {
+    type: String,
+    requires: true
+  },
+  totalAmount: {
+    type: String,
+    requires: true
+  }
+})
+module.exports = mongoose.model('Record', recordSchema)
