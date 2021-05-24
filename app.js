665be299c3f7468bc9bab112fd4107382c025c3e
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-24 16:39:21 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+// 資料庫連線
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true})
-
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -16,10 +16,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// Handlebars 設定
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 路由設定
 app.get('/', (req, res) => {
-  res.send('This is Web App!!')
+  res.render('index')
 })
 
+// 伺服器監聽
 app.listen(port, (req, res) => {
   console.log(`The server is listening on http://localhost${port}`)
 })
