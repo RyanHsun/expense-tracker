8d90e80d6ec7c3fef7692f30f0c1191ec3cab314
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-25 23:23:07 +0800

    feat: add total amount funtion & modify pages ui

--- routes/modules/filters.js
+++ routes/modules/filters.js
@@ -6,9 +6,14 @@ const Category = require('../../models/category')
 // 設定使用分類篩選支出的路由
 router.get('/', (req, res) => {
   const category = req.query.category
+  let totalAmount = 0
+
   Record.find({ category: category })
     .lean()
-    .then(records => {
+    .then((records) => {
+      for (let i = 0; i < records.length; i++) {
+        totalAmount += Number(records[i].amount)
+      }
       Category.find()
         .lean()
         .sort({ _id: 'asc' })
@@ -16,7 +21,7 @@ router.get('/', (req, res) => {
           if (records == 0) {
             res.render('error', { categories, category })
           } else {
-            res.render('index', { records, categories, category })
+            res.render('index', { records, categories, category, totalAmount })
           }
         })
     })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,14 +5,18 @@ const Category = require('../../models/category')
 
 // 設定瀏覽所有支出的路由 Read
 router.get('/', (req, res) => {
+  let totalAmount = 0
   Record.find()
     .lean()
     .sort({ date: 'desc' })
-    .then(records => {
+    .then((records) => {
+      for (let i = 0; i < records.length; i++) {
+          totalAmount += Number(records[i].amount)
+      }
       Category.find()
         .lean()
         .sort({ _id: 'asc' })
-        .then(categories => res.render('index', { records, categories }))
+        .then(categories => res.render('index', { records, categories, totalAmount }))
         .catch(error => console.error(error))
     })
     .catch(error => console.error(error))
